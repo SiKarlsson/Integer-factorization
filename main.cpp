58162b2b9d29931539af6507cb3f1dbbef3561eb
William Perkola <perkola.william@gmail.com>
2015-11-24 15:08:57 +0100

    Switch to string

--- main.cpp
+++ main.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
-#include <string>
 #include <sstream>
+#include <string>
 #include <vector>
-#include <math.h>
-#include <limits.h>
-#include <algorithm>
-#include <queue>
 
-std::vector<int> readNumbers() {
+/* Types */
+typedef unsigned long mp_limb_t;
+typedef long mp_size_t;
+typedef unsigned long mp_bitcnt_t;
+typedef mp_limb_t *mp_ptr;
+typedef const mp_limb_t *mp_srcptr;
+typedef struct {
+  int _mp_alloc;
+  int _mp_size;
+  mp_limb_t *_mp_d;
+} __mpz_struct;
+typedef __mpz_struct mpz_t[1];
 
-    std::vector<int> numbers;
-    int number;
+void mpz_init (mpz_t r) {
+  static const mp_limb_t dummy_limb = 0xc1a0;
+  r->_mp_alloc = 0;
+  r->_mp_size = 0;
+  r->_mp_d = (mp_ptr) &dummy_limb;
+}
+
+std::vector<std::string> readNumbers() {
+
+    std::vector<std::string> numbers;
+    std::string number;
     bool reading = true;
 
     while (reading) {
         std::cin >> number;
 
-        if (number != 0) {
+        if (number != "0") {
             numbers.push_back(number);
         } else {
             reading = false;
@@ -26,18 +42,20 @@ std::vector<int> readNumbers() {
     return numbers;
 }
 
-void writeNumbers(std::vector<int> numbers) {
-    
-    for (int i : numbers) {
+void writeNumbers(std::vector<std::string> numbers) {
+
+    for (std::string i : numbers) {
         std::cout << i << std::endl;
     }
 }
 
 int main() {
-    
-    std::vector<int> numbers = readNumbers();
+
+    std::vector<std::string> numbers = readNumbers();
+
+    //pollardsRho()
 
     writeNumbers(numbers);
 
-    return 0;   
+    return 0;
 }
