20f0415c5f31b3abc752be580a8f087572fb2daf
William Perkola <perkola.william@gmail.com>
2015-11-24 13:35:18 +0100

    Initial commit

---
+++ minify-ttmath.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cpp -m32 -P -nostdinc ttmath/ttmathuint.h | \
+	tr '\n' ' ' | \
+	sed 's/\s\+/ /g' | \
+	sed 's/\([^a-zA-Z0-9'\''_]\) /\1/g' | \
+	sed 's/ \([^a-zA-Z0-9'\''_%\($]\)/\1/g' | \
+	sed 's/^ //' | \
+	sed 's/\<return\>/R /g' | \
+	sed 's/\<operator\>/O/g' | \
+	sed 's/\<uint\>/U/g' | \
+	sed 's/\<UInt\>/V/g' | \
+	sed 's/\<value_size\>/S/g' | \
+	sed 's/\<const\>/C/g' | \
+	sed 's/\<table\>/T/g' | \
+	sed 's/std:://g' | \
+	sed 's/  / /g' | \
+	cat > incl.h && wc -c incl.h
---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define O operator
+#define R return
+#define C const
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#ifdef __x86_64__
+#error ttmath requires -m32
+#endif
+// ttmath, BSD licensed:
+#include "incl.h"
+// (paste incl.h here manually for submission to SPOJ)
+#pragma GCC diagnostic pop
+#undef C
+#undef R
+#undef O
